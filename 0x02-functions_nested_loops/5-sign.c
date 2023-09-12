e8cb41aad2322164bd561dd3a5649dc4bb7c9f2d
chengejr <chenge1.jr@gmail.com>
2023-09-12 12:51:21 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number
  * @n: The number to checked
  * Return: 1 for positive num, -1 for negative numb or zero for anything else
  */
