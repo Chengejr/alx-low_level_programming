b95cbc9f6417a98ec48f3b7f0d7a6891fe612ecf
chengejr <chenge1.jr@gmail.com>
2023-11-02 01:26:57 -0700

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,23 +10,18 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, status, i;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++);
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
 		return (-1);
-
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(fd, text_content, i);
-	if (status == -1)
-		return (-1);
-
-	close(fd);
+	close(o);
 	return (1);
 }
